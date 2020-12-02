a3cc1e986e2cf1984cd53673857d561493524aea
Grrrdolf <herseyb71@gmail.com>
2020-12-01 23:50:38 -0600

    Not ready to push yet still doing some changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
 var manipulated = document.querySelector('.manipulate');
 var filterControls = document.querySelectorAll('input[type=range]');
+var presetBtn = document.querySelector('.preset-btn');
 
-//console.log(filterControls) outputs the nodelist 
+presetBtn.addEventListener('click', createNewFilters)
+/*
+document.addEventListener("DOMContentLoaded", ()=> {
+    let wrapper = document.getElementById('wrapper');
+    let topLayer = wrapper.querySelector('.top');
+    let handle = wrapper.querySelector('.handle');
+    let skew = 0;
+    let delta = 0;
+
+
+    if(wrapper.className.indexOf('skewed') != -1){
+        skew = 1000;
+    }
+    
+    wrapper.addEventListener('mouseover', (e) => {
+        delta = (e.clientX - window.innerWidth / 2) * 0.5;
+    
+        handle.style.left = e.clientX + delta + 'px';
+
+        topLayer.style.width = e.clientX + skew + delta + 'px';
+    });
+})
+*/
+//console.log(filterControls) outputs the nodeList 
 
 function updateFilters(){
     let computerFilters = '';
@@ -12,6 +36,27 @@ function updateFilters(){
     console.log(computerFilters)
     manipulated.style.filter = computerFilters;
     
+}
+
+function createNewFilters(){
+    let html = '';
+    let preset = document.querySelector('.preset').parentElement.presetBtn;
+
+
+    html += `
+    <div class="presetFilters">
+        <div class="inner-content">
+            <button class="filterOption">Rose</button>
+            <button class="filterOption">Rose</button>
+            <button class="filterOption">Rose</button>
+            <button class="filterOption">Rose</button>
+        </div>
+    
+    </div>
+    `;
+    console.log(html);
+    preset.insertAdjacentElement('beforeend', html);
+
 }
 
 
